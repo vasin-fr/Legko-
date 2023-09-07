aba987369797fe4a0d001a1e82c7bc92eca4f44f
Oleksii Vasin <vasin.oleksii@gmail.com>
2023-09-07 13:45:41 +0200

    maket of application

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello wolrd! ❤️‍🔥</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Write task!" style={styles.textInput} />
+        <Button title="Add task!"></Button>
+      </View>
+      <View style={styles.taskContainer}>
+        <Text>List of tasks...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: "#fff",
+    padding: 50,
+    paddingHorizontal: 15,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "#cccccc",
+  },
+  textInput: {
+    padding: 10,
+    // justifyContent: "flex-end",
+    borderWidth: 2,
+    borderRadius: 5,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+  },
+  taskContainer: {
+    flex: 9,
   },
 });
